f752cf3988e43dd1d2bf1f2e1a0b466597e683b6
Alejandro Palacios <srjalean@gmail.com>
2024-05-27 11:59:57 +0200

    clean and refactoring code of master.php

--- master/index.php
+++ master/index.php
@@ -2,24 +2,14 @@
     include '../assets/reusable/sesions.php';
     include '../assets/reusable/bd.php';
 
-    $defaultImgPath = '../assets/images/img/user.jpg'; // Define una imagen por defecto
-
     $userId = $_SESSION['user_id'];
-    $sql = "SELECT * FROM users WHERE user_id = $userId"; // Consulta para obtener la imagen del perfil
+    $sql = "SELECT * FROM users WHERE user_id = $userId";
     $result = $db->query($sql);
-
     $row = $result->fetch_assoc();
-    $img = !empty($row['profile_img']) ? '../assets/usersImg/'.$row['profile_img'] : $defaultImgPath; // Usa la imagen de la DB o la por defecto
-
+    
+    $img = !empty($row['profile_img']) ? '../assets/usersImg/'.$row['profile_img'] : '../assets/images/img/user.jpg';
     $user_handle = $row['user_handle'];
     $first_name = $row['first_name'];
-
-    // Consulta para obtener los mensajes de soporte junto con la información del usuario
-    $sql = "SELECT support.support_id, support.message, support.created_at, users.user_handle, users.profile_img
-    FROM support
-    INNER JOIN users ON support.user_id = users.user_id
-    ORDER BY support.created_at DESC";
-    $result = $db->query($sql);
 
 
     function time_elapsed_string($datetime, $full = false) {
@@ -27,10 +17,8 @@
         $ago = new DateTime($datetime);
         $diff = $now->diff($ago);
     
-        // Calculamos las semanas y ajustamos los días
         $weeks = floor($diff->d / 7);
-        $remainingDays = $diff->d % 7; // Días restantes después de extraer las semanas
-    
+        $remainingDays = $diff->d % 7;
         $string = array(
             'y' => 'año',
             'm' => 'mes',
@@ -40,18 +28,15 @@
             'i' => 'minuto',
             's' => 'segundo',
         );
-    
-        // Asignamos los valores a las unidades de tiempo
         $diffValues = array(
             'y' => $diff->y,
             'm' => $diff->m,
-            'w' => $weeks,  // Usamos la variable local para semanas
+            'w' => $weeks,
             'd' => $remainingDays,
             'h' => $diff->h,
             'i' => $diff->i,
             's' => $diff->s
         );
-    
         foreach ($string as $k => &$v) {
             if ($diffValues[$k]) {
                 $v = $diffValues[$k] . ' ' . $v . ($diffValues[$k] > 1 ? 's' : '');
@@ -59,13 +44,12 @@
                 unset($string[$k]);
             }
         }
-    
         if (!$full) $string = array_slice($string, 0, 1);
         return $string ? 'hace ' . implode(', ', $string) : 'justo ahora';
     }
     
     
-    // -- DATOS DIV 3
+    // --- DATOS DIV 3 ---
     $sql_total_users = "SELECT COUNT(*) as total_users FROM users";
     $sql_total_posts = "SELECT COUNT(*) as total_posts FROM posts";
     $sql_active_users = "SELECT COUNT(*) as active_users FROM users WHERE active = 1";
@@ -85,7 +69,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <title>Blym</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -93,37 +76,34 @@
     <link rel="icon" href="../assets/images/svg/b.svg" type="image/x-icon">
 
     <!-- =============== CUSTOM CSS LINK =============== -->
-    <link rel="stylesheet" href="./styles.css?v=2">
+    <link rel="stylesheet" href="./styles.css">
 
     <!-- =============== Font Awesome Link =============== -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
-    <!-- =============== Swiper Slider Link =============== -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
 </head>
 <body>
 
     <!-- =============== Start Navbar =============== -->
     <nav>
-    <div class="container nav-container">
-        <div class="logo">
-            <h3>BLYM</h3>
-        </div>
-        <div class="search-bar" id="search-bar-div1">
-            <i class="fas fa-search"></i>
-            <input type="search" placeholder="Search Users" oninput="searchUsers(this.value)">
-        </div>
-        <div class="search-bar hidden" id="search-bar-div2">
-            <i class="fas fa-search"></i>
-            <input type="search" placeholder="Search Posts" oninput="searchPosts(this.value)">
-        </div>
+        <div class="container nav-container">
+            <div class="logo">
+                <h3>BLYM</h3>
+            </div>
+            <div class="search-bar" id="search-bar-div1">
+                <i class="fas fa-search"></i>
+                <input type="search" placeholder="Search Users" oninput="searchUsers(this.value)">
+            </div>
+            <div class="search-bar hidden" id="search-bar-div2">
+                <i class="fas fa-search"></i>
+                <input type="search" placeholder="Search Posts" oninput="searchPosts(this.value)">
+            </div>
 
-        <div class="profile-picture-nav" id="my-profile-picture">
-            <img src="<?php echo $img; ?>" alt="Profile Image">
+            <div class="profile-picture-nav" id="my-profile-picture">
+                <img src="<?php echo $img; ?>" alt="Profile Image">
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
     <!-- =============== Start Main Section =============== -->
     <main>
@@ -156,9 +136,9 @@
                      </a>
 
                      <a class="menu-item" id="btn3">
-                        <span><img src="../assets/images/svg/graph-up-arrow.svg" alt=""></span> <h3>Stats</h3>
+                        <span><img src="../assets/images/svg/graph-up-arrow.svg" alt=""></span> 
+                        <h3>Stats</h3>
                      </a>
-                
                 </aside>
             </div>
 
@@ -167,30 +147,7 @@
             <div class="main-middle">
                 <div class="middle-container">
                     <div id="div1" class="hidden users-grid"></div>
-                    <div id="div2" class="hidden">
-                        <?php
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                $userImg = !empty($row['profile_img']) ? '../assets/usersImg/' . $row['profile_img'] : $defaultImgPath;
-                                $timeAgo = time_elapsed_string($row['created_at']);
-                                ?>
-                                <div class="support-message">
-                                    <img src="<?php echo $userImg; ?>" alt="Profile Image" class="profile-image">
-                                    <p class="user-handle">@<?php echo $row['user_handle']; ?></p>
-                                    <p class="message-text"><?php echo $row['message']; ?></p>
-                                    <p class="time-ago"><?php echo $timeAgo; ?></p>
-                                    <button class='delete-btn' onclick="deleteMessage(<?php echo $row['support_id']; ?>)">
-                                        <i class='fas fa-trash delete-icon'></i>
-                                    </button>
-                                </div>
-                                <?php
-                            }
-                        } else {
-                            echo "<p>No hay mensajes de soporte para mostrar.</p>";
-                        }
-                        ?>
-                    </div>
-
+                    <div id="div2" class="hidden"></div>
                     <div id="div3" class="hidden">
                         <div class="stats-container">
                             <div class="stat-box">
@@ -229,7 +186,6 @@
         </div>
     </main>
 
-    <!-- =============== Start Popup Aria =============== -->
     <!-- =============== Start Profile-Popup =============== -->
     <div class="popup profile-popup">
         <div>
@@ -252,12 +208,7 @@
         </div>
     </div>
 
-
-
-    <!-- =============== Swiper JS Link =============== -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
     <!-- =============== Custom JS Link =============== -->
-    <script src="./scripts.js?v=2"></script>
+    <script src="./scripts.js"></script>
 </body>
 </html>
